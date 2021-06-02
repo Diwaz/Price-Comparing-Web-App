c203882517bca638a49a14054921ac4cb8d7c4a5
diwaz <diwaswatson@gmail.com>
2021-06-03 01:42:11 +0545

    Data scrapped
    data scrapped from search results

--- scrapper.js
+++ scrapper.js
@@ -3,8 +3,18 @@ const puppeteer = require('puppeteer');
 (async() => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://example.com');
-    const hrefElement = await page.$('a');
-    await hrefElement.click();
+    await page.goto('https://www.daraz.com.np/');
+    await page.click('[name=q]');
+    await page.keyboard.type("Watches");
+    await page.keyboard.press('Enter');
+    await page.waitForSelector('.c2prKC', { timeout: 10000 });
+    const [el] = await page.$x('//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div/div[2]/div[2]/a');
+    const text = await el.getProperty('textContent');
+    const name = await text.jsonValue();
+    console.log(name);
+    // const attr = await page.$$eval("a", el => el.map(x => x.getAttribute("title")));
+
+    //const hrefElement = await page.$x('//*[@id="Level_1_Category_No6"]/a/span');
+    //await hrefElement.click();
     // ...
 })();
