b11e76178216825c945ad43289611148a6ed9a61
diwaz <diwaswatson@gmail.com>
2021-06-06 23:39:29 +0545

    setting frontend
    setting up frontend using vanilla JavaScript

---
+++ indexx.js
@@ -0,0 +1,6 @@
+// var product = document.getElementById('Prod').value;
+const datas = document.getElementsByClassName('hid').innerHTML = 'hello';
+
+function searched() {
+    datas.innerHTML = product;
+}
