a2a918ba9efaabdecc64164f7b6843e29578a26e
diwaz <diwaswatson@gmail.com>
2021-06-06 23:53:52 +0545

    frontend basic setup

--- indexx.js
+++ indexx.js
@@ -1,6 +1,5 @@
-// var product = document.getElementById('Prod').value;
-const datas = document.getElementsByClassName('hid').innerHTML = 'hello';
-
 function searched() {
-    datas.innerHTML = product;
+    var product = document.getElementById('Prod').value;
+    //datas.innerHTML = product;
+    document.getElementById('hid').innerHTML = '1.' + product;
 }
