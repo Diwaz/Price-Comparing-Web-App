91df3ae4a3644ca15920bc1fee7cf5b40551b83a
diwaz <diwaswatson@gmail.com>
2021-06-07 23:21:55 +0545

    managing backend
    intilizing express

--- scrapper.js
+++ scrapper.js
@@ -49,17 +49,7 @@ async function checkPrice(page) {
     }
 
 
-    // const [el1] = await page.$x('//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div/div[2]/div[2]/a');
-    // const text1 = await el1.getProperty('textContent');
-    // const name = await text1.jsonValue();
-    // const [el] = await page.$x('//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div/div[2]/div[3]/span');
-    // const text2 = await el.getProperty('textContent');
-    // const price = await text2.jsonValue();
-    // console.log(name, price)
-    // return {
-    //     prodName,
-    //     prodPrice
-    // }
+
 }
 
 async function monitor() {
@@ -103,48 +93,6 @@ async function monitor() {
     }
 
 
-    // names = datas.prodName;
-    // price = datas.prodPrice;
-    // console.log(names, price);
 
 }
 monitor();
-
-
-
-
-
-
-
-
-
-
-// (async() => {
-
-
-//     const elements = await page.$$eval('.c2prKC', (options) =>
-//   options.map((option) => option.)
-// );
-
-// let elements = page.$$('.c2prKC');
-// console.log(elements[0])
-// for (i = 0; i < elements.length; i++) {
-//     console.log('(elements[i])');
-
-
-// }
-//     async page.$$eval('.c2prKC', (options) =>
-//         options.map((option) => {
-//                 const [pname] = await option.$x('//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div/div[2]/div[2]/a');
-//                 const jname = await pname.getProperty('textContent');
-//                 const name = await jname.jsonValue();
-//             }
-
-//         )
-//     );
-//     console.log(divCount);
-
-
-//     //console.log(name, price);
-
-// })();
