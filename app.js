cd8ddae9e5ea7f1e76c3ee9d13946637b3268f18
diwaz <diwaswatson@gmail.com>
2021-06-08 23:24:37 +0545

    adjusting scrapper
    adjusting scrapper to export to server

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const path = require('path');
 
-
+const scrapper = require('./scrapper');
 
 const app = express();
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/data', (req, res) => {
+    // res.sendFile(path.join(__dirname, 'index.html'));
+    res.json(scrapper.data);
 });
 
 app.listen(8080);
--- scrapper.js
+++ scrapper.js
@@ -1,20 +1,24 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const { next } = require('cheerio/lib/api/traversing');
 
 
 
-async function configureBrower() {
+async function scrapeData(product) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.daraz.com.np/');
-    return { page, browser };
-}
+    await page.goto('https://www.daraz.com.np/', {
+        waitUntil: 'load',
+        timeout: 0
+    });
 
-async function checkPrice(page) {
+    //}
+
+    //async function checkPrice(page) {
     await page.click('[name=q]');
-    await page.keyboard.type("Sanitizer");
+    await page.keyboard.type(product);
     await page.keyboard.press('Enter');
-    await page.waitForSelector('.c2prKC', { timeout: 10000 });
+    await page.waitForSelector('.c2prKC', { timeout: 30000 });
     let html = await page.evaluate(() => document.body.innerHTML);
 
     let $ = cheerio.load(html);
@@ -28,71 +32,77 @@ async function checkPrice(page) {
         let prodPrice = $(this).text();
         prices.push(prodPrice);
     });
-    const nameparse = naam => {
-        if (naam.length > 10) {
-            return naam.split(' ').slice(0, 5).join(" ");
-        }
-        return naam;
-    }
-    const looper = names.map(n => {
-            if (n.length > 10) {
-                return n.split(' ').slice(0, 5).join(" ");
-            }
-            return n;
-        })
-        // for (i = 0; i < 5; i++) {
-        //     console.log(nameparse(names[i]) + '...', prices[i]);
-        // }
+    // rname = JSON.stringify(names);
+    // rprod = JSON.stringify(prices);
+    // const nameparse = naam => {
+    //     if (naam.length > 10) {
+    //         return naam.split(' ').slice(0, 5).join(" ");
+    //     }
+    //     return naam;
+    // }
+    // const looper = names.map(n => {
+    //         if (n.length > 10) {
+    //             return n.split(' ').slice(0, 5).join(" ");
+    //         }
+    //         return n;
+    //     })
+    // for (i = 0; i < 5; i++) {
+    //     console.log(nameparse(names[i]) + '...', prices[i]);
+    // }
     return {
-        prices,
-        names
+        names,
+        prices
+
     }
-
-
-
 }
 
-async function monitor() {
-    let { browser } = await configureBrower();
-    try {
-        let { page } = await configureBrower();
-        const bundle = await checkPrice(page);
-        const total = (bundle.names).length;
 
-        console.log(total);
-        // //  var ran = bundle.names[3];
-        // //  var pan = bundle.prices[3];
-        // //  console.log(ran, pan)
-        //      var namesJ = {
-        //          productName: '',
-        //          productPrice: ''
-        //      }
-        //      for (i = 0; i < total; i++) {
+//}
+
+//async function monitor() {
+// let { browser } = await configureBrower();
+// try {
+//     let { page } = await configureBrower();
+//     const bundle = await checkPrice(page);
+//     const total = (bundle.names).length;
 
 
-        //          namesJ[i].productName= bundle.names[i],
-        //          namesJ[i].productPrice= bundle.prices[i]
-        //      }
+// //  var ran = bundle.names[3];
+// //  var pan = bundle.prices[3];
+// //  console.log(ran, pan)
+//      var namesJ = {
+//          productName: '',
+//          productPrice: ''
+//      }
+//      for (i = 0; i < total; i++) {
 
 
-
-        // const mainData = JSON.stringify(nameJ);
-        // console.log(mainData);
-        var package = [];
-        for (i = 0; i < total; i++) {
-            package.push(bundle.names[i]);
-            package.push(bundle.prices[i]);
-        }
-        const mainData = JSON.stringify(package);
-        console.log(mainData);
-
-    } catch {
-        console.log('error')
-    } finally {
-        await browser.close()
-    }
+//          namesJ[i].productName= bundle.names[i],
+//          namesJ[i].productPrice= bundle.prices[i]
+//      }
 
 
 
+// const mainData = JSON.stringify(nameJ);
+// console.log(mainData);
+//     var package = [];
+//     for (i = 0; i < total; i++) {
+//         package.push(bundle.names[i]);
+//         package.push(bundle.prices[i]);
+//     }
+//     const mainData = JSON.stringify(package);
+//     return mainData;
+
+// } catch {
+//     console.log('error')
+// } finally {
+//     await browser.close()
+// }
+// async function monitor() {
+//     const data = await scrapeData('sanitizer');
+//     console.log(data);
+// }
+// monitor();
+module.exports = {
+    scrapeData
 }
-monitor();
