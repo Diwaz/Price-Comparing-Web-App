e9fc3b8f038ec8a00b351828809bc431cac084b2
diwaz <diwaswatson@gmail.com>
2021-06-11 22:31:35 +0545

    bridging
    connecting backend to frontend

--- indexx.js
+++ indexx.js
@@ -1,3 +1,26 @@
+async function getData() {
+    // let header = new Headers({
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Content-Type': 'multipart/form-data'
+    // });
+    // let sentData = {
+    //     method: 'GET',
+    //     mode: 'cors',
+    //     header: header,
+    // };
+    console.log('started')
+    fetch('http://localhost:8080/data/api')
+        .then(resp => {
+            console.log(resp);
+        })
+        .catch(err => {
+            //console.log(err);
+
+        });
+
+
+}
+
 function searched() {
     var product = document.getElementById('Prod').value;
     //datas.innerHTML = product;
--- scrapper.js
+++ scrapper.js
@@ -15,12 +15,14 @@ async function scrapeData(product) {
     await page.click('[name=q]');
     await page.keyboard.type(product);
     await page.keyboard.press('Enter');
-    await page.waitForSelector('.c2prKC', { timeout: 10000 });
+    await page.waitForSelector('.c2prKC', { timeout: 20000 });
     let html = await page.evaluate(() => document.body.innerHTML);
 
     let $ = cheerio.load(html);
     const names = []
     const prices = []
+    const imgUrl = []
+    const link = []
     $('.c16H9d', html).each(function() {
         let prodName = $(this).text();
         names.push(prodName);
@@ -29,25 +31,47 @@ async function scrapeData(product) {
         let prodPrice = $(this).text();
         prices.push(prodPrice);
     });
+    $('.c1ZEkM', html).each(function() {
+
+        let prodImg = $(this).attr('src');
+        imgUrl.push(prodImg);
+    });
+    $('.cRjKsc', html).each(function() {
+
+        let prodLink = $(this).find('a').attr('href');
+        link.push(prodLink);
+    });
+
+    console.log(link);
 
     const result = await names.map((prodName, index) => {
 
         return {
             id: index,
             prodName,
-            prodPrice: prices[index]
+            prodPrice: prices[index],
+            imgUrl: imgUrl[index],
+            link: link[index]
         }
     });
+    browser.close();
     return result;
-    browser.close()
+
 }
+scrapeData('mask');
 
 // setting express server
 app = express();
+app.use((req, res, next) => {
+    res.setHeader('Allow-Control-Allow-Origin', '*');
+    res.setHeader('Allow-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Allow-Control-Allow-Header', 'Content-Type,Authorization');
+    next();
+});
 
 app.get('/data/api', async(req, res) => {
     try {
-        const prodData = await scrapeData('sanitizer');
+        const prodData = await scrapeData('mask');
         return res.status(200).json({
             data: prodData
         })
