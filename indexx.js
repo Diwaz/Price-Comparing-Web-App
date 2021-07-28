42409c943af907fa8e51e1b43f090bd5f8bd398e
diwaz <diwaswatson@gmail.com>
2021-07-28 22:59:30 +0545

    basic frontend setup w/o int price

--- indexx.js
+++ indexx.js
@@ -17,9 +17,10 @@ async function getData() {
     //     header: header,
     // };
     listing = document.getElementById('hid');
+    listing1 = document.getElementById('hid2');
     console.log('started')
     console.log(search)
-    fetch(`http://localhost:8080/data/api/${search}`)
+    fetch(`http://localhost:8080/data/api/daraz/${search}`)
         .then(res => res.json())
         .then(resText => {
             let html = '';
@@ -39,8 +40,39 @@ async function getData() {
 
 
 
+
             // };
             listing.innerHTML = html;
+
+        })
+
+    .catch(err => {
+        console.log(err);
+
+    });
+    fetch(`http://localhost:8080/data/api/sasto/${search}`)
+        .then(res => res.json())
+        .then(resText => {
+            let html1 = '';
+            console.log(resText)
+                // if (responseText.data) {
+            resText.data.forEach(product => {
+                html1 += `
+                <div>
+                <p>${product.id}</p>
+                <p>${product.prodName}</p>
+                <p>${product.prodPrice}</p>
+                <p>${product.site}</p>
+                </div>
+                </br>
+                `
+            })
+
+
+
+
+            // };
+            listing1.innerHTML = html1;
         })
 
     .catch(err => {
@@ -51,8 +83,8 @@ async function getData() {
 
 }
 
-function searched() {
-    var product = document.getElementById('Prod').value;
-    //datas.innerHTML = product;
-    document.getElementById('hid').innerHTML = '1.' + product;
-}
+// function searched() {
+//     var product = document.getElementById('Prod').value;
+//     //datas.innerHTML = product;
+//     document.getElementById('hid').innerHTML = '1.' + product;
+// }
--- scrapper.js
+++ scrapper.js
@@ -7,59 +7,60 @@ const axios = require('axios');
 
 
 
-// async function darazData(product) {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto('https://www.daraz.com.np/', {
-//         waitUntil: 'load',
-//         timeout: 0
-//     })
+async function darazData(product) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.daraz.com.np/', {
+        waitUntil: 'load',
+        timeout: 0
+    })
 
-//     await page.click('[name=q]');
-//     await page.keyboard.type(product);
-//     await page.keyboard.press('Enter');
-//     await page.waitForSelector('.c2prKC', { timeout: 20000 });
-//     let html = await page.evaluate(() => document.body.innerHTML);
+    await page.click('[name=q]');
+    await page.keyboard.type(product);
+    await page.keyboard.press('Enter');
+    await page.waitForSelector('.c2prKC', { timeout: 20000 });
+    let html = await page.evaluate(() => document.body.innerHTML);
 
-//     let $ = cheerio.load(html);
-//     const names = []
-//     const prices = []
-//     const imgUrl = []
-//     const link = []
-//     $('.c16H9d', html).each(function() {
-//         let prodName = $(this).text();
-//         names.push(prodName);
-//     });
-//     $('.c3gUW0', html).each(function() {
-//         let prodPrice = $(this).text();
-//         prices.push(prodPrice);
-//     });
-//     $('.c1ZEkM', html).each(function() {
+    let $ = cheerio.load(html);
+    const names = []
+    const prices = []
+    const imgUrl = []
+    const link = []
+    $('.c16H9d', html).each(function() {
+        let prodName = $(this).text();
+        names.push(prodName);
+    });
+    $('.c3gUW0', html).each(function() {
+        let prodPrice = $(this).text();
+        prices.push(prodPrice);
+    });
+    $('.c1ZEkM', html).each(function() {
 
-//         let prodImg = $(this).attr('src');
-//         imgUrl.push(prodImg);
-//     });
-//     $('.cRjKsc', html).each(function() {
+        let prodImg = $(this).attr('src');
+        imgUrl.push(prodImg);
+    });
+    $('.cRjKsc', html).each(function() {
 
-//         let prodLink = $(this).find('a').attr('href');
-//         link.push(prodLink);
-//     });
+        let prodLink = $(this).find('a').attr('href');
+        link.push(prodLink);
+    });
 
 
-//     const result = await names.map((prodName, index) => {
+    const result = await names.map((prodName, index) => {
 
-//         return {
-//             id: index,
-//             prodName,
-//             prodPrice: prices[index],
-//             imgUrl: imgUrl[index],
-//             link: link[index]
-//         }
-//     });
-//     browser.close();
-//     return result;
+        return {
+            id: index,
+            prodName,
+            prodPrice: prices[index],
+            imgUrl: imgUrl[index],
+            link: link[index],
+            site: 'daraz'
+        }
+    });
+    browser.close();
+    return result;
 
-// }
+}
 async function sastoData(product) {
     //    
     const browser = await puppeteer.launch({
@@ -75,7 +76,7 @@ async function sastoData(product) {
     // await page.keyboard.press('Enter');
     // await navigationPromise;
     await page.waitForSelector('.item:nth-child(1) > .product-item-info > .product > .product > .product-item-link')
-        // await page.waitForSelector('.product-item-info', { timeout: 20000 });
+
     let html = await page.evaluate(() => document.body.innerHTML);
 
     let $ = cheerio.load(html);
@@ -102,41 +103,12 @@ async function sastoData(product) {
         link.push(prodLink);
     });
 
-    // const relateArticle = $('.product .details .product-item-details').map((i, section) => {
-    //     let articles = $(section).find('a');
-    //     return articles.text().trim();
-    // }).get(0)
-
-    // console.log(relateArticle) //Foo
-
-    // for (let l = 0; l < 5; l++) {
-    //     let prodName = $$('.product-item-link')[l];
-    //     names.push(prodName);
-    // }
-
-    // const oreo = await page.$$eval('.product-item-link', (options) =>
-    //     options.map((option) => option.textContent)
-    // );
-    // console.log(oreo);
 
 
 
 
 
-    // $('.c3gUW0', html).each(function() {
-    //     let prodPrice = $(this).text();
-    //     prices.push(prodPrice);
-    // });
-    // $('.c1ZEkM', html).each(function() {
 
-    //     let prodImg = $(this).attr('src');
-    //     imgUrl.push(prodImg);
-    // });
-    // $('.cRjKsc', html).each(function() {
-
-    //     let prodLink = $(this).find('a').attr('href');
-    //     link.push(prodLink);
-    // });
 
 
     const result = await names.map((prodName, index) => {
@@ -154,7 +126,7 @@ async function sastoData(product) {
     return result;
 
 }
-// sastoData('gaming headphone');
+
 
 
 
@@ -168,14 +140,28 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Header', 'Content-Type,Authorization');
     next();
 });
-// app.post('/data/api/:product',async(req,res)=>{
 
-// })
-app.get(`/data/api/:product`, async(req, res) => {
+app.get(`/data/api/sasto/:product`, async(req, res) => {
+    let draft = req.params.product;
+    console.log(draft);
+    let prod = draft.split('%20').join(' ');
+
+    try {
+        const prodData = await sastoData(prod);
+        return res.status(200).json({
+            data: prodData
+        })
+    } catch (err) {
+        return res.status(500).json({
+            err: err,
+        })
+    }
+})
+app.get(`/data/api/daraz/:product`, async(req, res) => {
     let prod = req.params.product;
     console.log(prod);
     try {
-        const prodData = await sastoData(prod);
+        const prodData = await darazData(prod);
         return res.status(200).json({
             data: prodData
         })
